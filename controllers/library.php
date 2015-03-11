b2e10c03299c6f552ca7c06d36c23fa39fa35d43
jauhmerah <jauhmerah@gmail.com>
2015-03-11 20:40:11 +0800

    Update library.php

--- controllers/library.php
+++ controllers/library.php
@@ -5,7 +5,9 @@ class Library extends CI_Controller
 	function index()
 	{
 		$this->load->view("libHome");		
-	}	
+	}
+	
+	//>>>>>>>>Staff Function>>>>>>>>>>
 	function loginStaff()
 	{
 		echo "hai login staff";
@@ -19,6 +21,8 @@ class Library extends CI_Controller
 			$this->load->view("staffPage");
 		}		
 	}
+	
+	//>>>>>>>>User Function>>>>>>>>>>	
 	function loginUser()
 	{
 		echo "hai login user";
@@ -32,6 +36,20 @@ class Library extends CI_Controller
 			$this->load->view("userPage");
 		}		
 	}
+	function registerUser()
+	{
+		$userID = $this->input->post('userID');
+		$password = $this->input->post('pass');
+		$name = $this->input->post('name');
+		$address = $this->input->post('address');
+		$this->load->model("lib_model");
+		if($this->lib_model->insertUser($userID,$password, $name , $address))
+			echo "<br />Register Done";
+		else
+			echo "<br>Register Error";
+	}
+	
+	//>>>>>>>>Borrow Function>>>>>>>>>>	
 	function approvalLoanProcess()
 	{
 		$userID = $this->input->post('userID');
@@ -42,7 +60,7 @@ class Library extends CI_Controller
 			$row = $this->lib_model->searchBook($bookID);
 			if($row->available != 0)
 			{
-				$this->lib_model->setBookAvailable($row);
+				$this->lib_model->setBookAvailable($row , -1);
 				$this->lib_model->insertLoan($userID , $bookID);
 			}
 			else
@@ -50,10 +68,46 @@ class Library extends CI_Controller
 				echo "Book was Not available";	
 			}
 		}
+	}	
+	function returnProcess()
+	{
+		$userID = $this->input->post('userID');
+		$bookID = $this->input->post('bookCode');
+		$this->load->model("lib_model");
+		$data = $this->lib_model->getLoan($userID,$bookID);
+		$this->load->helper("date");
+		$loanDayTime = $data->loanDate;	
+		$today = new DateTime(date("y-m-d",now()));
+		$loanDayTime = new DateTime($loanDayTime);
+		$loanDay = new DateTime($loanDayTime->format("y-m-d"));//remove time "h:i:s		
+		$diff = $loanDay->diff($today);
+		$diff = $diff->format("%a days");//%R for +ve , %a for diff
+		$data = array(
+			"userID" => $userID,
+			"bookID" => $bookID,
+			"loanDay" => $loanDay->format("d-m-y"),
+			"loanDayTime" => $data->loanDate,
+			"amountOFDay" =>$diff
+			);
+		$this->load->view("returnApprovePage",$data);		
+		//date_timestamp_get(date_create());
+		//echo date_default_timezone_get();
+	}
+	function deleteBorrowProcess()
+	{
+		$userID = $this->input->post('userID');
+		$bookID = $this->input->post('bookID');
+		$loanDate = $this->input->post('loanDayTime');
+		$data = array(
+			   	'ic' => $userID ,
+			   	'bookID' => $bookID ,
+				'loanDate' => $loanDate			   			   
+			);
+		$this->load->model("lib_model");
+		$this->lib_model->deleteLoan($data);
 	}
 	
-	//>>>>>>>>>>>>>>>>>>>>>Direct page>>>>>>>>>>>>>>>>>>>>>
-	
+	//>>>>>>>>>>>>>>>>>>>>>Direct page>>>>>>>>>>>>>>>>>>>>>	
 	function directUserPage()
 	{
 		$this->load->view("userPage");
@@ -65,6 +119,14 @@ class Library extends CI_Controller
 	function directLoanPage()
 	{
 		$this->load->view("loanPage");
+	}
+	function directRegisterPage()
+	{
+		$this->load->view("registerPage");
+	}
+	function directReturnPage()
+	{
+		$this->load->view("returnPage");
 	}
 }
 
